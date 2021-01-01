5a8aa91c0df690ca381c6be2b9e838157359fca3
bmike7 <bijlmike3@gmail.com>
2021-01-01 14:04:25 +0100

    ThreadPool: webserver can implement multiple threads to do different tasks;

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-pub struct ThreadPool;
+use std::fmt;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+type Result<T> = std::result::Result<T, PoolCreationError>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -8,14 +18,51 @@ impl ThreadPool {
     /// # panics
     ///
     /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-        ThreadPool
+    pub fn new(size: usize) -> Result<ThreadPool> {
+        match size {
+            0 => Err(PoolCreationError),
+            _ => {
+                let (sender, receiver) = mpsc::channel();
+                let receiver = Arc::new(Mutex::new(receiver));
+                let mut workers = Vec::with_capacity(size);
+                for id in 0..size {
+                    workers.push(Worker::new(id, Arc::clone(&receiver)));
+                }
+                Ok(ThreadPool { workers, sender })
+            }
+        }
     }
 
-    pub fn execute<F>(&self, _f: F)
+    pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    handler: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let handler = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} got a job; executing...", id);
+            job();
+        });
+        Worker { id, handler }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PoolCreationError;
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid number of threads")
     }
 }
